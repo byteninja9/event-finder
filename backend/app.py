3efea279c350464fb902632c330b8865ffa454f6
𝓢𝓪𝓶𝓼𝓸𝓷 <124459773+byteninja9@users.noreply.github.com>
2024-06-28 04:39:08 +0300

    Remove Api Key

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ def search_events():
         "q": query,
         "hl": "en",
         "gl": "us",
-        "api_key": "e5318aeada3e98d7108db4a1b6c234d5e1261736108e00404b51d051cb99c86d"
+        "api_key": "your_serpapi_key"
     }
 
     searchs = search(params)
